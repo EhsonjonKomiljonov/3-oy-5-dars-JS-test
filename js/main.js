d248e56b2ef14f265f556904cacddccf8e7cd52e
EhsonjonKomiljonov <ekomiljonov29@gmail.com>
2022-11-24 15:03:09 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,62 @@
+var userName = prompt("Ismingizni kiriting");
+alert("Assalomu Alaykum" + " " + userName);
+
+// Test 1
+
+var test1 = prompt(
+  "transform: rotate( ); nima ishni bajaradi? \n a)O'ngga, Chapga yurgazadi; \n b)O'ngga, Chapga, Tepaga, Pastga yurgazadi; \n c)Orqaga, yonga, Z o'qi bo'yicha qayiltiradi yotqizadi; \n d)Faqat (Z) o'qi bo'yicha harakatlantiradi;"
+);
+
+console.log("Test-1 \n" + "User answer:" + test1 + "\n" + "Correct answer: c");
+
+// Test 1
+
+// Test 2
+
+var test2 = prompt(
+  "animation-ittaration-count; nima ishni bajaradi? \n a)elementi boshida yoki oxirida qanaqa holatta bo'lishini belgilaydi; \n b)Yumshoq harakatlantirib beradi; \n c)@keyframes; ga yozilgan (%)larni qanday holatta o'qilishini belgilaydi; \n d)Qadamlar sonini belgilaydi;"
+);
+
+console.log("Test-2 \n" + "User answer:" + test2 + "\n" + "Correct answer: d");
+
+// Test 2
+
+// Test-3
+
+var test3 = prompt(
+  "transform: matrix( ); o'z ichiga qaysi qiymatlarni oladi? \n a)scaleX( ),scaleY( ),skewX( ),skewY( ),translateX( );\n b)scaleX( ),skewX( ),skewY( ),scaleY( ),translateX( ),translateY( ); \n c)skewX( ),scaleX( ),scaleY( ),skewY( ),translateX( ),translateY( ); \n d)translateX( ),translateY( ),skewX( ),skewY( ),scaleX( ),scaleY( );"
+);
+
+console.log("Test-3 \n" + "User answer:" + test3 + "\n" + "Correct answer: b");
+
+// Test-3
+
+// Test-4
+
+var test4 = prompt(
+  "animation larimiz uchun nimani ichiga u bajaradigan narsalarni foizini yozamiz? \n a)@keyframe{} \n b)@keyfrname{} \n c)@keyfr{} \n d)@keyrframes{}"
+);
+
+console.log("Test-4 \n" + "User answer:" + test4 + "\n" + "Correct answer: d");
+
+// Test-4
+
+// Test-5
+
+var test5 = prompt(
+  "biza VScode da yengi branch ochishimiz uchun qaysi terminal code dan foydalanamiz? \n a)git branch 'branchname' \n b)git branch -b 'branchname' \n c)git checkout -b 'branchname' \n d)git checkout 'branchname"
+);
+
+console.log("Test-5 \n" + "User answer:" + test5 + "\n" + "Correct answer: c");
+
+// Test-5
+
+// Test-6
+
+var test6 = prompt(
+  "object-position nima ishni bajaradi? \n a)otasini width+height ga qarab rasmni sifati buzilmidigan qilib to'g'irlidi \n b)otasi bo'ylab rasmni harakatlantiradi \n c)rasm otasiga sig'maganda qayog'i ko'rinishi kerakligini belgilidi \n d)b va c javoblari to'g'ri"
+);
+
+console.log("Test-6 \n" + "User answer:" + test6 + "\n" + "Correct answer: c");
+
+// Test-6
